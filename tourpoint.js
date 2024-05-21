8269c39e3c280819175f689e0365cdfdc713a202
PitchayapaPim <158021156+PitchayapaPim@users.noreply.github.com>
2024-05-21 23:33:09 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pa Hin Ngam National Park",
+        "type": "natural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          101.39432299999856,
+          15.632530929717674
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pha Hua Nak Viewpoint",
+        "type": "natural",
+        "level": 4.7
+      },
+      "geometry": {
+        "coordinates": [
+          101.94902408598324,
+          16.056456972203023
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pha Hum Hod",
+        "type": "natural",
+        "level": 4.6
+      },
+      "geometry": {
+        "coordinates": [
+          101.45436827116316,
+          15.896890617958462
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Mo Hin Khao",
+        "type": "natural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          101.96647867857337,
+          16.06392566975977
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ChulaBhorn Dam",
+        "type": "natural",
+        "level": 4.6
+      },
+      "geometry": {
+        "coordinates": [
+          101.64930884655541,
+          16.535299046380814
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tat Ton Waterfall",
+        "type": "natural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          102.04061375099394,
+          15.984059257299833
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Thung Sawang",
+        "level": 4,
+        "type": "cultural"
+      },
+      "geometry": {
+        "coordinates": [
+          102.07143972883597,
+          15.819982580618358
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Khao Bang Hoei Chumphon Simaram",
+        "type": "cultural",
+        "level": 4.6
+      },
+      "geometry": {
+        "coordinates": [
+          101.4351275423279,
+          15.822328935721956
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Don La Nam",
+        "type": "special",
+        "level": 4.1
+      },
+      "geometry": {
+        "coordinates": [
+          101.92600827116394,
+          15.629204265901294
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "์Nong Pla Thao Park",
+        "type": "special",
+        "level": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          102.01046552380478,
+          15.80179788650905
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
